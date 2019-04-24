bbe27d3f7a8d72f6b0a07a734c618568116674d3
Kyle <kyle@ekrosenberg.com>
2019-04-24 12:43:33 +0000

    Deploying app...

--- src/interfaces/ISpotifyTokenRequest.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ISpotifyTokenRequest {
-  client_id: string;
-  response_type: "token";
-  redirect_uri: string;
-  state?: string;
-  scopes?: string[];
-  show_dialog?: boolean;
-}
--- src/interfaces/ISpotifyTokenResponse.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ISpotifyTokenResponse {
-  access_token: string;
-  token_type: "Bearer";
-  expires_in: string;
-  state?: string;
-}
